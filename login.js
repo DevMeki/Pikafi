a9ff3c0774316fbe8164cde50801c5ced7412e6a
mmaduabuchi <emmanuelukwe1@gmail.com>
2025-01-28 16:53:09 +0100

    added the login page backend

--- login.js
+++ login.js
@@ -2,25 +2,13 @@ let registerBtn = document.querySelector("#register");
 
 registerBtn.addEventListener("click", function () {
     let email = document.querySelector("#email").value;
-    let username = document.querySelector("#username").value;
     let password = document.querySelector("#password").value;
-    let confirm_password = document.querySelector("#confirm_password").value;
     let error_text = document.querySelector(".error_text");
 
     if (email == "") {
         error_text.innerHTML = "Email is empty";
-    } else if (username == "") {
-        error_text.innerHTML = "Username is empty";
     } else if (password == "") {
         error_text.innerHTML = "Password is empty";
-    } else if (confirm_password == "") {
-        error_text.innerHTML = "Confirm password is empty";
-    } else if (confirm_password !== password) {
-        error_text.innerHTML = "Password does not match";
-    } else if (password.length < 8) {
-        error_text.innerHTML = "Password is weak (must be 8 characters and above)";
-    } else if (!isNaN(password)) {
-        error_text.innerHTML = "Password must contain alphanumeric characters";
     } else {
         // confirm_password
         error_text.innerHTML = "";
@@ -29,8 +17,8 @@ registerBtn.addEventListener("click", function () {
         // Make an AJAX request
         $.ajax({
             type: "POST",
-            url: "process_register.php", // replace with your PHP script URL
-            data: "email=" + encodeURIComponent(email) + "&username=" + encodeURIComponent(username) + "&password=" + encodeURIComponent(password) + "&confirm_password=" + encodeURIComponent(confirm_password),
+            url: "process_login.php", // replace with your PHP script URL
+            data: "email=" + encodeURIComponent(email) + "&password=" + encodeURIComponent(password),
             // data: form_data,
             dataType: "json",
 
@@ -39,12 +27,7 @@ registerBtn.addEventListener("click", function () {
 
                 if (responseData.status === 'info') {
                     // Display information for 'info' status
-                    Swal.fire({
-                        title: 'Information',
-                        text: responseData.message,
-                        icon: 'info',
-                        confirmButtonText: 'OK'
-                    });
+                    error_text.innerHTML = responseData.message;
                 } else if (responseData.status === 'success') {
                     // Handle success status
                     Swal.fire({
@@ -55,17 +38,12 @@ registerBtn.addEventListener("click", function () {
                     }).then((result) => {
                         if (result.isConfirmed) {
                             // Simulate a mouse click:
-                            window.location.href = "login.html";
+                            window.location.href = "Home.html";
                         }
                     });
                 } else {
                     // Handle other statuses
-                    Swal.fire({
-                        title: 'Status: ' + responseData.status,
-                        text: responseData.message,
-                        icon: 'info',
-                        confirmButtonText: 'OK'
-                    });
+                    error_text.innerHTML = responseData.message;
                 }
             },
             error: function (error) {
--- process_login.php
+++ process_login.php
@@ -1,73 +1,64 @@
 <?php
-    // Start the session
-    session_start();
+// Start the session
+session_start();
 
-    //connect to database
-    include "databaseConnection.php";
+//connect to database
+include "databaseConnection.php";
 
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        //exit script outputting json data
-        $output = json_encode(
-            array(
-                'status' => 'error',
-                'message' => 'failed'
-            )
-        );
-        // die($output);
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    //exit script outputting json data
+    $output = json_encode(
+        array(
+            'status' => 'error',
+            'message' => 'failed'
+        )
+    );
+    // die($output);
 
-        //get users input
-        $username = $_POST["userName"];
-        $password = $_POST["password"];
+    //get users input
+    $email = $_POST["email"];
+    $password = $_POST["password"];
 
-        if (empty($username)) {
-            // echo "username is empty";
-            $output = json_encode(array('status' => 'error', 'message' => 'username is empty'));
+    if (empty($email)) {
+        // echo "username is empty";
+        $output = json_encode(array('status' => 'error', 'message' => 'Email is empty'));
+        die($output);
+    } else {
+        if (empty($password)) {
+            // echo "phone is empty";
+            $output = json_encode(array('status' => 'error', 'message' => 'Password is empty'));
             die($output);
-        }else{
-            if (empty($password)) {
-                // echo "phone is empty";
-                $output = json_encode(array('status' => 'error', 'message' => 'password is empty'));
+        } else {
+            $fetchUser = "SELECT * FROM `users` WHERE email = '$email'";
+            $query = mysqli_query($conn, $fetchUser);
+            if (!$query) {
+                $output = json_encode(array('status' => 'error', 'message' => 'An error occured....'));
                 die($output);
-            }else{
-                $fetchUser = "SELECT * FROM users WHERE Username = '$username'";
-                $query = mysqli_query($conn, $fetchUser);
-                if (!$query) {
-                    $output = json_encode(array('status' => 'error', 'message' => 'User does not exist.'));
+            } else {
+                if (mysqli_num_rows($query) < 1) {
+                    $output = json_encode(array('status' => 'error', 'message' => 'This user does not exist.'));
                     die($output);
-                }else {
+                } else {
                     $dataAsArray = mysqli_fetch_assoc($query);
-                    $verifyPassword = password_verify($password, $dataAsArray["Password"]);
+                    $verifyPassword = password_verify($password, $dataAsArray["password"]);
                     if ($verifyPassword) {
-                        if ($dataAsArray["Auth"] == "user") {
-                            $_SESSION['username'] = $username;
+                        if ($dataAsArray["auth"] === "user") {
                             $_SESSION['userToken'] = $dataAsArray["userToken"];
-                            $_SESSION['Auth'] = $dataAsArray["Auth"];
-                            $_SESSION['UserInvitationCode'] = $dataAsArray["InvitationCode"];
+                            $_SESSION['username'] = $dataAsArray["username"];
+                            $_SESSION['email'] = $dataAsArray["email"];
+                            $_SESSION['auth'] = $dataAsArray["auth"];
                             $output = json_encode(array('status' => 'success', 'message' => 'success'));
                             die($output);
-                        }else if($dataAsArray["Auth"] == "subadmin"){
-                            //subadmin
-                            $_SESSION['username'] = $username;
-                            $_SESSION['userToken'] = $dataAsArray["userToken"];
-                            $_SESSION['Auth'] = $dataAsArray["Auth"];
-                            $_SESSION['UserInvitationCode'] = $dataAsArray["InvitationCode"];
-                            $output = json_encode(array('status' => 'successAuth', 'message' => 'success'));
-                            die($output);
-                        }else{
-                            $_SESSION['username'] = $username;
-                            $_SESSION['userToken'] = $dataAsArray["userToken"];
-                            $_SESSION['Auth'] = $dataAsArray["Auth"];
-                            $_SESSION['UserInvitationCode'] = $dataAsArray["InvitationCode"];
-                            $output = json_encode(array('status' => 'successAuth', 'message' => 'success'));
+                        } else {
+                            $output = json_encode(array('status' => 'info', 'message' => 'Users allowed only..'));
                             die($output);
                         }
-                    }else{
-                        $output = json_encode(array('status' => 'error', 'message' => 'Incorrect username or password'));
+                    } else {
+                        $output = json_encode(array('status' => 'info', 'message' => 'Incorrect email or password'));
                         die($output);
                     }
                 }
             }
         }
-
     }
-?>
+}
